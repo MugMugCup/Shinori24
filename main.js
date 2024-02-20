2c051ac63e408e204b5b57ae50c89dc1ad4a8b93
MugMugCup <139062914+MugMugCup@users.noreply.github.com>
2024-02-20 19:23:59 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,12 @@
     let x_pos = 1930;
     let moving = false;
     const ship = document.getElementById('ship');
+    const event = document.getElementById('event');
 
     window.addEventListener('keypress', function(e)
     {
         go_ship();
+        hide_event();
     });
 
     function go_ship()
@@ -30,6 +32,26 @@
             moving = false;
             stop = false;
             console.log('stop');
+            disp_event();
         }
+
+        if(x_pos < -306)
+        {
+            clearInterval(moving);
+            moving = false;
+            x_pos = 1930;
+            ship.style.left = x_pos + 'px';
+            console.log('respawn');
+        }
+    }
+
+    function disp_event()
+    {
+        event.style.display = 'block';
+    }
+
+    function hide_event()
+    {
+        event.style.display = 'none';
     }
 } 
